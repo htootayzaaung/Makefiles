2f9c4472128f53e94ad997d221fe15847df0cabc
Htoo Aung <sc21hta@leeds.ac.uk>
2022-09-01 00:59:54 +0630

    Simple_C++_Makefile

---
+++ Simple_C++_Makefile/message.cpp
@@ -0,0 +1,13 @@
+#include  <iostream>
+#include "message.hpp"
+
+Message::Message():a(2)
+{
+	std::cout << "constructor" << std::endl;
+}
+
+void Message::print_hello()
+{
+	std::cout << "\nHello World\n" << std::endl;
+}
+
---
+++ Simple_C++_Makefile/message.hpp
@@ -0,0 +1,14 @@
+#ifndef SIMPLE_Cpp_MAKEFILE
+#define SIMPLE_Cpp_MAKEFILE
+
+class Message
+{
+	public:
+		Message();
+		void print_hello();
+	
+	private:
+		int a;
+};
+
+#endif 
